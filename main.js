a64e03767a5173c611453b5ce7c9b6a24414f079
mAabduqayum <mabduqayum@gmail.com>
2022-02-06 17:23:11 +0500

    minor changes

---
+++ main.js
@@ -0,0 +1,38 @@
+const toUpper = () => {
+    let matn = document.getElementById("idOfText").value;
+    document.getElementById("idOfText").value = matn.toUpperCase();
+}
+const toLower = () => {
+    let matn = document.getElementById("idOfText").value;
+    document.getElementById("idOfText").value = matn.toLowerCase();
+}
+const toProper = () => {
+    let matn = document.getElementById("idOfText").value;
+    document.getElementById("idOfText").value =
+        matn.replace(/(\w)(\w*)/g, (match, g1, g2) => {
+            return g1.toUpperCase() + g2.toLowerCase();
+        })
+}
+const toSentence = () => {
+    let matn = document.getElementById("idOfText").value;
+    matn = matn.toLowerCase();
+    document.getElementById('idOfText').value =
+        matn.replace(/([\n.] ?)(\w)/g, (match, g1, g2) => {
+            return g1 + g2.toUpperCase();
+        });
+}
+
+function download() {
+    let filename = "text.txt";
+    let text = document.getElementById("idOfText").value
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
